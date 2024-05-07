d19c628190db1a89575cd9b5acbaa85b941dd974
Chris Johnson <chrispy1002@gmail.com>
2024-05-07 02:05:33 -0400

    Added the dynamic ToC
    This is a prototype that has the major functionality for the dynamic Table of Contents. It contains links to the major sections of the page, and will highlight the sections that are visible on screen.

--- chrispy-website/src/App.js
+++ chrispy-website/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import DynamicToC from './components/DynamicToC';
+import Section from './components/Section';
+import React from 'react';
 
 function App() {
+
+  // TODO: Automate this!
+  const SectionData = [
+    {
+      s_id: "temp1",
+      s_name: "Temp 1"
+    },
+    {
+      s_id: "temp2",
+      s_name: "Temp 2"
+    },
+    {
+      s_id: "temp3",
+      s_name: "Temp 3"
+    },
+    {
+      s_id: "temp4",
+      s_name: "Temp 4"
+    }
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">                   { /* Main React App */ }
+      <div>
+        <Section s_id = {"temp1"} s_name = {"Temp 1"}></Section>
+        <Section s_id = {"temp2"} s_name = {"Temp 2"}></Section>
+        <Section s_id = {"temp3"} s_name = {"Temp 3"}></Section>
+        <Section s_id = {"temp4"} s_name = {"Temp 4"}></Section>
+      </div>
+      
+
+      <DynamicToC SectionData={SectionData}></DynamicToC>
     </div>
   );
 }
